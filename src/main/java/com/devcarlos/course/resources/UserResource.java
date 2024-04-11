fe7facd7bc653bb1a3cf27cf483146a01d85b708
Carlos Fernandes <itais1@hotmail.com>
2024-04-12 00:27:57 +0100

    Delete

--- src/main/java/com/devcarlos/course/resources/UserResource.java
+++ src/main/java/com/devcarlos/course/resources/UserResource.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -40,9 +41,16 @@ public class UserResource {
 	public ResponseEntity<User> insert(@RequestBody User obj) {
 		obj = service.insert(obj);
 		
+		// esta inha de URI é para ter a resposta 201 emves de 200
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
 				.buildAndExpand(obj.getId()).toUri();
 		
 		return ResponseEntity.created(uri).body(obj);
 	}
+	@DeleteMapping(value = "/{id}") // quando vamos deletar temos que passaar o id
+	public ResponseEntity<Void> delete(@PathVariable Long id) { // é void porque nao vai retorar nada e usamos  o @PathVariable para que este meto possa reconecer o variavel id 
+		service.delete(id);
+       return ResponseEntity.noContent().build(); // o noContent() ele vai tornar para nos uma resposta vazia e codigo http de uma resposta que nao tem conteudo é o 204
+	}
+	
 }
