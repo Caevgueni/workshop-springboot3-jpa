2cee2d3a36aab98f11055835eb5d0434392601c8
Carlos Fernandes <itais1@hotmail.com>
2024-04-12 15:50:06 +0100

    Exception handling - delete

--- src/main/java/com/devcarlos/course/services/UserService.java
+++ src/main/java/com/devcarlos/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Component;
 
 import com.devcarlos.course.entities.User;
 import com.devcarlos.course.repositories.UserRepository;
+import com.devcarlos.course.services.exceptions.DatabaseException;
 import com.devcarlos.course.services.exceptions.ResourceNotFoundException;
 
 @Component // assim registamos o UserService como componente do spring para que as outras
@@ -36,8 +39,15 @@ public class UserService {
 	}
 
 	public void delete(Long id) {
+		
+		try {
 
 		repository.deleteById(id);
+		} catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) { // isto quer dizer que qualquer outra excecao que vier a ocorrer que imprima o seguinte mensagem
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 
 	public User update(Long id, User obj) { // é do tipo User porque vai retornar usuaria atualizado, com id de usaurio
